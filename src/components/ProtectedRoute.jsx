7a0bba15af522fe3ae40222d5e8f97d348bc32a2
Hilina Teshome <teshilina8@gmail.com>
2024-12-09 19:02:35 +0300

    Updates

--- src/components/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({ children }) => {
-    const userToken = localStorage.getItem("userToken");
-
-    return userToken ? children : <Navigate to="/login" />;
-};
-
-export default ProtectedRoute;
