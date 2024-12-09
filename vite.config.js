11f6694f6c9e486f3ae908494be098920584f96c
Hilina Teshome <teshilina8@gmail.com>
2024-12-09 19:38:36 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Ecommerce_Website_Desktop/',
   plugins: [react()],
   eslint: {
     ignoreDuringBuilds : true,
