c213b9ed9a2722070f2fa3e4c73c05f4caf83525
Hilina Teshome <teshilina8@gmail.com>
2024-12-09 19:58:01 +0300

    base url changed

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Ecommerce_Website_Desktop/',
+  base: '/',
   plugins: [react()],
   eslint: {
     ignoreDuringBuilds : true,
